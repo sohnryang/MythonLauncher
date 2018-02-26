1c2de33dd2bde5a1dc0e6bef432a922c8a49309a
Egnima <chooheonoh@gmail.com>
2018-02-26 22:59:20 +0900

    Mython Launcher

---
+++ Mython_Launcher/Mython_Launcher/MainForm.Designer.cs
@@ -0,0 +1,224 @@
+﻿using System;
+
+namespace Mython_Launcher
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.progressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.statusLbl = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnLaunch = new System.Windows.Forms.Button();
+            this.btnConfig = new System.Windows.Forms.Button();
+            this.btnInstall = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.progressBar,
+            this.statusLbl});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 216);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(275, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(100, 16);
+            // 
+            // statusLbl
+            // 
+            this.statusLbl.Name = "statusLbl";
+            this.statusLbl.Size = new System.Drawing.Size(54, 17);
+            this.statusLbl.Text = "statusLbl";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(122, 40);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "서버 실행";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnLaunch
+            // 
+            this.btnLaunch.Location = new System.Drawing.Point(12, 68);
+            this.btnLaunch.Name = "btnLaunch";
+            this.btnLaunch.Size = new System.Drawing.Size(122, 40);
+            this.btnLaunch.TabIndex = 2;
+            this.btnLaunch.Text = "Minecraft 실행";
+            this.btnLaunch.UseVisualStyleBackColor = true;
+            this.btnLaunch.Click += new System.EventHandler(this.btnLaunch_Click);
+            // 
+            // btnConfig
+            // 
+            this.btnConfig.Location = new System.Drawing.Point(140, 12);
+            this.btnConfig.Name = "btnConfig";
+            this.btnConfig.Size = new System.Drawing.Size(122, 40);
+            this.btnConfig.TabIndex = 3;
+            this.btnConfig.Text = "서버 설정 열기";
+            this.btnConfig.UseVisualStyleBackColor = true;
+            this.btnConfig.Click += new System.EventHandler(this.btnConfig_Click);
+            // 
+            // btnInstall
+            // 
+            this.btnInstall.Location = new System.Drawing.Point(140, 68);
+            this.btnInstall.Name = "btnInstall";
+            this.btnInstall.Size = new System.Drawing.Size(122, 40);
+            this.btnInstall.TabIndex = 4;
+            this.btnInstall.Text = "Python IDE 실행";
+            this.btnInstall.UseVisualStyleBackColor = true;
+            this.btnInstall.Click += new System.EventHandler(this.btnInstall_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox3);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 115);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(250, 98);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "서버 설정";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(110, 66);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 20);
+            this.comboBox3.TabIndex = 8;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(110, 40);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 20);
+            this.comboBox2.TabIndex = 7;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "difficulty : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "level-type : ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "online-mode : ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(110, 14);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(275, 238);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnInstall);
+            this.Controls.Add(this.btnConfig);
+            this.Controls.Add(this.btnLaunch);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.statusStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Launcher";
+            this.Load += new System.EventHandler(this.Launcher_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLbl;
+        private System.Windows.Forms.ToolStripProgressBar progressBar;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnLaunch;
+        private System.Windows.Forms.Button btnConfig;
+        private System.Windows.Forms.Button btnInstall;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Mython_Launcher/Mython_Launcher/MainForm.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.ComponentModel;
+using System.IO.Compression;
+using System.Diagnostics;
+using System.Text;
+
+namespace Mython_Launcher
+{
+    public partial class MainForm : Form
+    {
+        static string MythonDir = Environment.CurrentDirectory + @"\Mython";
+        static string serverDir = MythonDir + @"\Minecraft Tools\server\server.properties";
+        static string Mython_URL = "https://www.dropbox.com/s/r7hdlyff4870j12/Mython.zip?dl=1";
+        static string Mython_Path = MythonDir + ".zip";
+
+        String[] cb1 = { "true", "false" };
+        String[] cb2 = { "DEFAULT", "FLAT", "LARGEBIOMES", "AMPLIFIED" };
+        String[] cb3 = { "peaceful", "easy", "normal", "difficult" };
+
+        bool isOpen = false;
+        bool isDonwload = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            this.MaximumSize = this.Size;
+            this.MinimumSize = this.Size;
+            this.MaximizeBox = false;
+            Timer timer = new System.Windows.Forms.Timer();
+            timer.Interval = 500;
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+        }
+
+        private void Launcher_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.AddRange(cb1);
+            comboBox2.Items.AddRange(cb2);
+            comboBox3.Items.AddRange(cb3);
+            comboBox1.SelectedIndex = 1;
+            comboBox2.SelectedIndex = 1;
+            comboBox3.SelectedIndex = 0;
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            DirectoryInfo DI = new DirectoryInfo(MythonDir);
+            if (DI.Exists)
+            {
+                if (isDonwload != true)
+                {
+                    statusLbl.ForeColor = System.Drawing.Color.LimeGreen;
+                    statusLbl.Text = "폴더 존재";
+                }
+            }
+            else
+            {
+                btnOpen.Enabled = false;
+                btnLaunch.Enabled = false;
+                btnConfig.Enabled = false;
+                btnInstall.Enabled = false;
+
+                if (isDonwload != true)
+                {
+                    statusLbl.ForeColor = System.Drawing.Color.Red;
+                    statusLbl.Text = "폴더 없음";
+                }
+                if (isOpen == false)
+                {
+                    isOpen = true;
+
+                    if (MessageBox.Show("폴더가 존재하지 않습니다. 파일을 다운받으시겠습니까?",
+                        "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error) == DialogResult.OK)
+                    {
+                        // 파일 다운로드
+                        DI.Create();
+                        fileDownload();
+                        isOpen = false;
+                    }
+                }
+            }
+        }
+
+        void fileDownload()
+        {
+            WebClient wc = new WebClient();
+            wc.DownloadFileCompleted += new System.ComponentModel.AsyncCompletedEventHandler(downCompleted);
+            wc.DownloadProgressChanged += new DownloadProgressChangedEventHandler(progressChanged);
+            wc.DownloadFileAsync(new Uri(Mython_URL), Mython_Path);
+        }
+
+        private void progressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            isDonwload = true;
+            progressBar.Value = e.ProgressPercentage;
+            statusLbl.ForeColor = System.Drawing.Color.Black;
+            statusLbl.Text = e.ProgressPercentage.ToString() + "%";
+        }
+
+        private void downCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            ZipFile.ExtractToDirectory(Mython_Path, MythonDir);
+            statusLbl.Text = "압축 푸는중...";
+            statusLbl.ForeColor = System.Drawing.Color.Black;
+            FileInfo fi = new FileInfo(Mython_Path);
+            DirectoryInfo di = new DirectoryInfo(MythonDir);
+            fi.Delete();
+            if (di.Exists == true)
+            {
+                MessageBox.Show("다운로드가 완료되었습니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            btnOpen.Enabled = true;
+            btnLaunch.Enabled = true;
+            btnConfig.Enabled = true;
+            btnInstall.Enabled = true;
+            isDonwload = false;
+        }
+
+        private void OpenFile(string dir)
+        {
+            try
+            {
+                Process.Start(dir);
+            }
+            catch (Win32Exception win32)
+            {
+                MessageBox.Show(win32.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Mython 폴더를 지우고 다시 실행해 주세요", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void OpenFile(string dir, string arg)
+        {
+            Process ps = new Process();
+            try
+            {
+                ps.StartInfo.FileName = dir;
+                ps.StartInfo.RedirectStandardInput = true;
+                ps.StartInfo.RedirectStandardOutput = true;
+                ps.StartInfo.CreateNoWindow = true;
+                ps.StartInfo.UseShellExecute = false;
+                ps.Start();
+                ps.StandardInput.WriteLine(arg);
+                ps.StandardInput.Close();
+                ps.WaitForExit();
+            }
+            catch (Win32Exception win32)
+            {
+                MessageBox.Show(win32.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Mython 폴더를 지우고 다시 실행해 주세요", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFile("CMD.exe", @"cd Mython\Minecraft Tools\server && start start.bat"); // Server
+        }
+
+        private void btnLaunch_Click(object sender, EventArgs e)
+        {
+            OpenFile("CMD.exe", @"cd Mython && start run.bat"); // Minecraft
+        }
+
+        private void btnConfig_Click(object sender, EventArgs e)
+        {
+            OpenFile(MythonDir + @"\Minecraft Tools\server\server.properties"); // Server config
+        }
+
+        private void btnInstall_Click(object sender, EventArgs e)
+        {
+            OpenFile(MythonDir + @"\Python\WinPython Interpreter"); // Python
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // http://infodbbase.tistory.com/113 참조
+            try
+            {
+                FileHandler("online-mode=", comboBox1.Text);
+            }
+            catch { }
+        }
+
+        private void FileHandler(string key, string value)
+        {
+            string[] textValue = File.ReadAllLines(serverDir);
+            string temp = "";
+            foreach (string s in textValue)
+            {
+                string t = s;
+                if (t.Contains(key))
+                {
+                    t = key + value;
+                }
+                temp += t + Environment.NewLine;
+            }
+            File.WriteAllText(serverDir, temp, Encoding.Default);
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                FileHandler("level-type=", comboBox2.Text);
+            }
+            catch { }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox3.Text == cb3[0])
+                    FileHandler("difficulty=", "0");
+                else if (comboBox3.Text == cb3[1])
+                    FileHandler("difficulty=", "1");
+                else if (comboBox3.Text == cb3[2])
+                    FileHandler("difficulty=", "2");
+                else if (comboBox3.Text == cb3[3])
+                    FileHandler("difficulty=", "3");
+            }
+            catch { }
+        }
+    }
+}
